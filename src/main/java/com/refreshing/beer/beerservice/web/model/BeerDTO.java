d7fd98d6f83951d689e8da549d089af383bacd73
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-04-04 17:31:30 +0530

    Adding JSR validations for BeerDTO. Closes #10

--- src/main/java/com/refreshing/beer/beerservice/web/model/BeerDTO.java
+++ src/main/java/com/refreshing/beer/beerservice/web/model/BeerDTO.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,17 +18,31 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class BeerDTO {
+
+    @Null //This will make sure whenever this POJO is coming from UI, this field always has to be null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @NotNull
+    @Positive
     private Long upc;
 
+    @Positive
+    @NotNull
     private BigDecimal price;
 
     private Integer quantityOnHand;
