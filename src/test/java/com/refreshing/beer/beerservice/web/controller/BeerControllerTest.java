1031686b5f836daca920c578c18fe75de450e027
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-04-05 20:21:04 +0530

    Fixing test failures. Closes #19

--- src/test/java/com/refreshing/beer/beerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/refreshing/beer/beerservice/web/controller/BeerControllerTest.java
@@ -1,18 +1,22 @@
 package com.refreshing.beer.beerservice.web.controller;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.refreshing.beer.beerservice.bootstrap.BeerLoader;
+import com.refreshing.beer.beerservice.services.BeerService;
 import com.refreshing.beer.beerservice.web.model.BeerDTO;
 import com.refreshing.beer.beerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,8 +29,13 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerService beerService;
+
     @Test
     void getBeerById() throws Exception {
+        given(beerService.getBeerById(any())).willReturn(getValidBeerDTO());
+
         mockMvc.perform(get(BeerController.API_V1_BEER + "/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
@@ -34,6 +43,8 @@ class BeerControllerTest {
     @Test
     void saveNewBeer() throws Exception {
         BeerDTO beerDTO = getValidBeerDTO();
+
+        given(beerService.saveNewBeer(any())).willReturn(getValidBeerDTO());
 
         String beerDtoJson = objectMapper.writeValueAsString(beerDTO);
         mockMvc.perform(post(BeerController.API_V1_BEER)
@@ -58,7 +69,7 @@ class BeerControllerTest {
                 .beerName("My beer")
                 .beerStyle(BeerStyleEnum.PALE_ALE)
                 .price(new BigDecimal("2.99"))
-                .upc(1231121212121L)
+                .upc(BeerLoader.BEER_1_UPC)
                 .build();
     }
 }
