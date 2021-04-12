f086c034ff1b19c344b92ee2f18618e6bb6e3971
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-04-12 11:29:27 +0530

    Fixing startup issue due to DDL hibernate. Closes #35.

--- src/main/java/com/refreshing/beer/beerservice/BeerServiceApplication.java
+++ src/main/java/com/refreshing/beer/beerservice/BeerServiceApplication.java
@@ -2,6 +2,7 @@ package com.refreshing.beer.beerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class BeerServiceApplication {
