b4674bac0e53efa5122b1f5871d36ae27268b2ba
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-04-13 18:58:40 +0530

    Adding support for messaging (using JMS by Active MQ Artemis). Closes #40.

---
+++ src/main/java/com/refreshing/beer/beerservice/config/JmsConfig.java
@@ -0,0 +1,20 @@
+package com.refreshing.beer.beerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter mappingJackson2MessageConverter = new MappingJackson2MessageConverter();
+        mappingJackson2MessageConverter.setTargetType(MessageType.TEXT);
+        mappingJackson2MessageConverter.setTypeIdPropertyName("_type");
+
+        return mappingJackson2MessageConverter;
+    }
+}
